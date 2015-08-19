69e758326f4c57f37709c45d6cabfa19d5c394e6
Onwaba <onwaba@projectcodex.co>
2015-08-19 10:39:40 +0200

    fixed sales support

--- index.js
+++ index.js
@@ -1,147 +1,131 @@
-var express = require("express"),
-    exphbs = require("express-handlebars"),
-    mysql = require("mysql"),
-    bodyParser = require("body-parser"),
-    myConnection = require("express-myconnection"),
-    spaza = require("./routes/spaza");    
-    
+ var express = require("express"),
+     exphbs = require("express-handlebars"),
+     mysql = require("mysql"),
+     bodyParser = require("body-parser"),
+     myConnection = require("express-myconnection"),
+     spaza = require("./routes/spaza");    
+     
+ 
+     products = require("./routes/products");
+     categories = require("./routes/categories");
+     addPurchases = require("./routes/purchases")
+     //addProducts = require("./routes/products");
+     sales = require("./routes/sales");
+     suppliers = require("./routes/suppliers");
+ 
+         
+ var app = express();
+ 
+ var dbOptions = {
+     host : "localhost",
+     user : "root",
+    // password : "2197832",
+     password : "amila",
+     port : 3306,
+     database : "sakonwaba"
+ };
+ 
+ app.engine("handlebars", exphbs({defaultLayout : "main"}));
+ app.set("view engine", "handlebars");
+ 
+ app.use(express.static(__dirname + "/public"));
+ 
+ // allows you to use mysql from the http request
+ app.use(myConnection(mysql, dbOptions, "single"));
+ 
+ //gives the request the ability to handle form parameters
+ app.use(bodyParser.urlencoded({ extended : false}))
+ app.use(bodyParser.json());
+ 
+ //app.get("/",products.show);
 
-    products = require("./routes/products");
-    categories = require("./routes/categories");
-    purchases = require("./routes/purchases")
-    //addProducts = require("./routes/products");
-    sales = require("./routes/sales");
-    suppliers = require("./routes/suppliers");
 
-        
-var app = express();
-
-var dbOptions = {
-    host : "localhost",
-    user : "root",
-    password : "amila",
-    port : 3306,
-    database : "sakonwaba"
-};
-
-app.engine("handlebars", exphbs({defaultLayout : "main"}));
-app.set("view engine", "handlebars");
-
-app.use(express.static(__dirname + "/public"));
-
-// allows you to use mysql from the http request
-app.use(myConnection(mysql, dbOptions, "single"));
-
-//gives the request the ability to handle form parameters
-app.use(bodyParser.urlencoded({ extended : false}))
-app.use(bodyParser.json());
-
-//app.get("/",products.show);
+ 
+  app.get("/Products", products.showProductList);
 
 
+ 
  app.get("/Products", products.showProductList);
+ //app.get("/add",Products.show)
+ //app.get("/products/edit/:id", products.get);
+ //app.get("/products/add/", products.showAdd);
+ 
+ //app.get("/products", products.showProductList);
+ //app.get("/products/add/:id", products.showAdd);
+ 
+ //app.get("/add",products.show);
 
 
-app.get("/Products", products.showProductList);
-//app.get("/add",Products.show)
-//app.get("/products/edit/:id", products.get);
-//app.get("/products/add/", products.showAdd);
-
-//app.get("/products", products.showProductList);
-//app.get("/products/add/:id", products.showAdd);
-
-//app.get("/add",products.show);
-
- app.get("/products/edit/:id", products.get);
- app.get("/products/add/:id", products.showAdd);
- app.post("/products/add/",products.add);
- app.post("/products/update/:id", products.update);
- app.get("/products/delete/:id", products.delete);
-
-app.get("/popular_products", products.showpopularPdt);
-app.get("/least_products", products.showleastPdt);
+  app.get("/products/edit/:id", products.get);
+  app.get("/products/add/:id", products.showAdd);
+  app.post("/products/add/",products.add);
+  app.post("/products/update/:id", products.update);
+  app.get("/products/delete/:id", products.delete);
+ 
+ app.get("/popular_products", products.showpopularPdt);
+ app.get("/least_products", products.showleastPdt);
 
 
-
-
-//app.get("/products/edit/:id", products.get);
-//app.post("/products/update/:id", products.update);
-
-//app.get("/products/delete/:id", products.delete);
-
-
-//app.get("/categories", categories.show);
+ //app.get("/products/edit/:id", products.get);
+ //app.post("/products/update/:id", products.update);
+ 
+ //app.get("/products/delete/:id", products.delete);
+ 
+ 
+ //app.get("/categories", categories.show);
 app.get("/sales",sales.show);
-<<<<<<< HEAD
 app.get("/sales/update/:id", sales.get);
 app.get("/sales/edit/:id", sales.showEdit);
+//app.get("/sales/update/:id", sales.get)
 app.get("/sales/update/:id", sales.get)
 app.get("/sales/add/", sales.showAdd);
 app.post("/sales/add/", sales.addsale);
 app.post("/sales/edit/", sales.salesUpdate);
+app.post("/sales/update/",sales.salesUpdate);
 app.post("/sales/update/:id",sales.salesUpdate);
-=======
-//app.get("/sales/update/:id", sales.get);
-//app.get("/sales/edit/:id", sales.showEdit);
-//app.get("/sales/update/:id", sales.get)
-//app.get("/sales/add/", sales.showAdd);
-app.post("/sales/update/:id",sales.update);
-app.post("/sales/add", sales.add);
-app.get("/sales/edit/:id", sales.get);
->>>>>>> b2027164d9547aa5e23c51cd0967df787fb8fadc
 app.get("/sales/delete/:id",sales.delete);
-
-
-app.get("/categories", categories.show);
-app.post("/categories/add", categories.add);
-app.get("/categories/edit/:id", categories.get);
-app.post("/categories/update/:id",categories.update);
-app.get("/categories/delete/:id",categories.delete);
-
-app.get("/popular_category",categories.showmostPopCat);
-app.get("/least_category",categories.showleastPopCat);
-
-app.get("/product_earnings",spaza.showearningsPerPdt);
-app.get("/profitable_product",spaza.showmostProfPdt);
-
-
-<<<<<<< HEAD
-app.get("/purchases",purchases.show);
-//app.post("/purchases/add", purchases.showAdd);
-app.post("/purchases/update/:id", purchases.update);
-app.post("/purchases/add", purchases.add);
-app.get("/purchases/edit/:id", purchases.get);
-app.get("/purchases/delete/:id", purchases.delete);
-=======
-app.get("/addPurchases",addPurchases.show);
-app.get("/addPurchases/add/", addPurchases.showAdd);
-app.get("/products/add/:id", products.showAdd);
-app.post("/addPurchases/add/",addPurchases.add);
-//app.post("/addPurchases/update/:id", addPurchases.update);
-//app.post("/addPurchases/add/:id", addPurchases.add);
-//app.get("/addPurchases/delete/:id", addPurchases.delete);
->>>>>>> ec993064f7348011cfb791a448a88d9b4c831936
-
-
-app.get('/suppliers',suppliers.show);
-app.post('/suppliers/update/:id',suppliers.update);
-app.post('/suppliers/add',suppliers.add);
-app.get('/suppliers_edit/:id', suppliers.get);
-app.get('/suppliers/delete/:id', suppliers.delete);
-
-
-//app.get();
-
-app.get("/",function (req, res){
-res.render("index");
-});
-
-
-var port = process.env.PORT || 8080;       
-   //start the server
-var server = app.listen(port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-       console.log('Example app listening at http://%s:%s', host, port);
-});
-
+ 
+ 
+ app.get("/categories", categories.show);
+ app.post("/categories/add", categories.add);
+ app.get("/categories/edit/:id", categories.get);
+ app.post("/categories/update/:id",categories.update);
+ app.get("/categories/delete/:id",categories.delete);
+ 
+ app.get("/popular_category",categories.showmostPopCat);
+ app.get("/least_category",categories.showleastPopCat);
+ 
+ app.get("/product_earnings",spaza.showearningsPerPdt);
+ app.get("/profitable_product",spaza.showmostProfPdt);
+ 
+ 
+//app.get("/addPurchases",addPurchases.show);
+//app.get("/addPurchases/add/", addPurchases.showAdd);
+//app.get("/products/add/:id", products.showAdd);
+//app.post("/addPurchases/add/",addPurchases.add);
+ //app.post("/addPurchases/update/:id", addPurchases.update);
+ //app.post("/addPurchases/add/:id", addPurchases.add);
+ //app.get("/addPurchases/delete/:id", addPurchases.delete);
+ 
+ 
+ app.get('/suppliers',suppliers.show);
+ app.post('/suppliers/update/:id',suppliers.update);
+ app.post('/suppliers/add',suppliers.add);
+ app.get('/suppliers_edit/:id', suppliers.get);
+ app.get('/suppliers/delete/:id', suppliers.delete);
+ 
+ 
+ //app.get();
+ 
+ app.get("/",function (req, res){
+ res.render("index");
+ });
+ 
+ 
+ var port = process.env.PORT || 8080;       
+    //start the server
+ var server = app.listen(port, function () {
+     var host = server.address().address;
+     var port = server.address().port;
+        console.log('Example app listening at http://%s:%s', host, port);
+ });
--- routes/sales.js
+++ routes/sales.js
@@ -1,159 +1,304 @@
 exports.show = function (req, res, next) {
-	req.getConnection(function(err, connection){
-		if (err)
-			return next(err);
-	
+ 	req.getConnection(function(err, connection){
+ 		if (err)
+ 			return next(err);
+ 	
+			connection.query("SELECT DATE_FORMAT(sales.date,'%d %b %y') as date, sales.id as id,sales.quantity as Quantity, sales.prod_id as id,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
 			connection.query("SELECT sales.id as id, DATE_FORMAT(sales.date,'%d %b %y') as date, sales.id as id,sales.quantity as Quantity, sales.prod_id as prod_id,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
-	        	if (err) 
-	        		return next(err);
-		var query = 'SELECT name,id from products';
-		//var query = 'SELECT sales.prod_id as id,products.name as name from sales,products WHERE products.id = sales.prod_id group by name';
-		connection.query(query, [], function(err, results1){
-		    //connection.query(query, [], function(err, results) {
-			//if (err) 
-				//return next(err);
-				//console.log(products);
-
-			
-			
-
-			//console.log(req.query);
-			//console.log("---> " + req.query);
-
-
-			//if(req.query.error === "true" ){
-				//result.error = "Can't delete category..."
-			//}
-
-				res.render( 'sales', {
-					sales : results,
-					products : results1
-				});
-			});
-		});
-	});
+ 	        	if (err) 
+ 	        		return next(err);
+ 		    var query = 'SELECT name,id from products';
+ 		//var query = 'SELECT sales.prod_id as id,products.name as name from sales,products WHERE products.id = sales.prod_id group by name';
+ 		    connection.query(query, [], function(err, results1){
+ 		    
+ 
+ 				  res.render( 'sales', {
+ 					sales : results,
+ 					products : results1
+ 				});
+ 			});
+ 		});
+ 	    });
+    });
 };
-/*
 exports.showAdd = function (req, res, next) {
-
-		req.getConnection(function(err, connection){
-		if (err)
-			return next(err);
-	
+ 
+ 		req.getConnection(function(err, connection){
+ 		if (err)
+ 			return next(err);
+ 	
+			connection.query("SELECT DATE_FORMAT(sales.date,'%d %b %y') as date, sales.quantity as Quantity, sales.prod_id as id,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
 			connection.query("SELECT sales.id as id, DATE_FORMAT(sales.date,'%d %b %y') as date, sales.quantity as Quantity, sales.prod_id as prod_id,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
-	        	if (err) 
-	        		return next(err);
-		var query = 'SELECT name,id from products';
-		//var query = 'SELECT sales.prod_id as id,products.name as name from sales,products WHERE products.id = sales.prod_id group by name';
-		connection.query(query, [], function(err, results1){
-				res.render( 'addSaleScreen', {
-					sales : results,
-					products : results1
-				});
-			});
-		});
+ 	        	if (err) 
+ 	        		return next(err);
+ 		var query = 'SELECT name,id from products';
+ 		//var query = 'SELECT sales.prod_id as id,products.name as name from sales,products WHERE products.id = sales.prod_id group by name';
+ 		connection.query(query, [], function(err, results1){
+ 				res.render( 'addSaleScreen', {
+ 					sales : results,
+ 					products : results1
+ 				});
+ 			});
+ 		});
+ 	});
 	});
-};
+ };
 exports.showEdit = function (req, res, next) {
-
-		req.getConnection(function(err, connection){
-		if (err)
-			return next(err);
-	
+ 
+ 		req.getConnection(function(err, connection){
+ 		if (err)
+ 			return next(err);
+ 	
+			connection.query("SELECT DATE_FORMAT(sales.date,'%d %b %y') as date, sales.quantity as Quantity, sales.prod_id as id,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
 			connection.query("SELECT sales.id, DATE_FORMAT(sales.date,'%d %b %y') as date, sales.quantity as Quantity, sales.prod_id as prod_id ,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
-	        	if (err) 
-	        		return next(err);
-		var query = 'SELECT name,id from products';
-		//var query = 'SELECT sales.prod_id as id,products.name as name from sales,products WHERE products.id = sales.prod_id group by name';
-		connection.query(query, [], function(err, results1){
-				res.render( 'editSales', {
-					sales : results,
-					products : results1
-				});
-			});
-		});
-	});
+ 	        	if (err) 
+ 	        		return next(err);
+ 		var query = 'SELECT name,id from products';
+ 		//var query = 'SELECT sales.prod_id as id,products.name as name from sales,products WHERE products.id = sales.prod_id group by name';
+ 		connection.query(query, [], function(err, results1){
+ 				res.render( 'editSales', {
+ 					sales : results,
+ 					products : results1
+ 				});
+ 			});
+ 		});
+ 	});
+    });
 };
-*/
-exports.add = function (req, res, next) {
-	req.getConnection(function(err, connection){
-		if (err){
-			return next(err);
-		}
-		var input = JSON.parse(JSON.stringify(req.body));
-		var data = {
+exports.addsale = function (req, res, next) {
+ 	req.getConnection(function(err, connection){
+ 		if (err){
+ 			return next(err);
+ 		}
+ 		var input = JSON.parse(JSON.stringify(req.body));
+ 		var data = {
+ 			prod_id: input.prod_id,
+ 			date: input.sales_date,
+ 			quantity: input.quantity,
+ 			price : input.price
+ 		};
+ 		connection.query('insert into sales set ?', data, function(err, results) {
+ 			if (err)
+ 			    console.log("Error inserting : %s ",err );
+ 
+ 			res.redirect('/sales');
+ 		});
+ 	});
+};
+ 
+ 
+ exports.get = function(req, res, next){
+ 	var id = req.params.id;
+ 	var input = JSON.parse(JSON.stringify(req.body));
+ 	    var data = {
+ 			prod_id: input.id,
+ 			date: input.date,
+ 			quantity: input.quantity,
+ 			price : input.price
+ 		};
+ 	req.getConnection(function(err, connection){
+ 		connection.query('SELECT * FROM sales WHERE id = ?', [id], function(err,rows){
+ 			if(err){
+ 		        console.log("Error Selecting : %s ",err );
+ 			}
+ 		    res.render('editSales',{page_title:"Edit Customers - Node.js", data : rows[0]});
+ 		});
+ 	});
+ };
+ 
+ exports.salesUpdate = function(req, res, next){
+ 	var data = JSON.parse(JSON.stringify(req.body));
+ 	var id = req.params.id;
+ 	var input = JSON.parse(JSON.stringify(req.body));
+ 	     var data = {
 			prod_id: input.id,
-			date: input.sales_date,
+			date: input.date,
 			quantity: input.quantity,
-			price : input.price
-		};
-		connection.query('insert into sales set ?', data, function(err, results) {
-			if (err)
-			    console.log("Error inserting : %s ",err );
-
-			res.redirect('/sales');
-		});
-	});
-};
-
-
-exports.get = function(req, res, next){
-	var id = req.params.id;
-	var input = JSON.parse(JSON.stringify(req.body));
-	    var data = {
-			prod_id: input.id,
-			date: input.sales_date,
-			quantity: input.quantity,
-			price : input.price
-		};
-	req.getConnection(function(err, connection){
-		connection.query('SELECT * FROM sales WHERE id = ?', [id], function(err,rows){
-			if(err){
-		        console.log("Error Selecting : %s ",err );
-			}
-		    res.render('/editSales',{page_title:"Edit Customers - Node.js", data : rows[0]});
-		});
-	});
-};
-
-exports.update = function(req, res, next){
-	var data = JSON.parse(JSON.stringify(req.body));
-	var id = req.params.id;
-	var input = JSON.parse(JSON.stringify(req.body));
-	     var data = {
-<<<<<<< HEAD
+			price : input.price,
 			name: input.name
 			//date: input.date,
 			//quantity: input.quantity,
 			//price : input.price
-=======
+ 		};
+ 	req.getConnection(function(err, connection){
+ 		connection.query('UPDATE sales SET ? WHERE id = ?', [data, id], function(err, rows){
+ 			if (err){
+ 		       console.log("Error Updating : %s ",err );
+ 			}
+ 		    res.redirect('/sales');
+ 		});
+ 	});
+ };
+ exports.delete = function(req, res, next){
+ 	var id = req.params.id;
+ 	req.getConnection(function(err, connection){
+ 	    connection.query('DELETE FROM sales WHERE Id = ?', [id], function(err,rows){
+ 	        if(err){
+ 	            console.log("Error Selecting : %s ",err );
+ 	            return res.redirect('/categories?error=true&msg=category_linked');
+ 		    }
+ 		    else{
+ 	        	res.redirect('/sales');
+ 	        }
+ 		});
+ 	});
+ }; 
+exports.show = function (req, res, next) {
+ 	req.getConnection(function(err, connection){
+ 		if (err)
+ 			return next(err);
+ 	
+			connection.query("SELECT DATE_FORMAT(sales.date,'%d %b %y') as date, sales.id as id,sales.quantity as Quantity, sales.prod_id as id,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
+			connection.query("SELECT sales.id as id, DATE_FORMAT(sales.date,'%d %b %y') as date, sales.id as id,sales.quantity as Quantity, sales.prod_id as prod_id,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
+ 	        	if (err) 
+ 	        		return next(err);
+ 		var query = 'SELECT name,id from products';
+ 		//var query = 'SELECT sales.prod_id as id,products.name as name from sales,products WHERE products.id = sales.prod_id group by name';
+ 		connection.query(query, [], function(err, results1){
+ 		    //connection.query(query, [], function(err, results) {
+ 			//if (err) 
+ 				//return next(err);
+ 				//console.log(products);
+ 
+ 			
+ 			
+ 
+ 			//console.log(req.query);
+ 			//console.log("--> " + req.query);
+ 
+ 
+ 			//if(req.query.error === "true" ){
+ 				//result.error = "Can't delete category..."
+ 			//}
+ 
+ 				res.render( 'sales', {
+ 					sales : results,
+ 					products : results1
+ 				});
+ 			});
+ 		});
+ 	});
+	});
+};
+exports.showAdd = function (req, res, next) {
+ 
+ 		req.getConnection(function(err, connection){
+ 		if (err)
+ 			return next(err);
+ 	
+			connection.query("SELECT DATE_FORMAT(sales.date,'%d %b %y') as date, sales.quantity as Quantity, sales.prod_id as id,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
+			connection.query("SELECT sales.id as id, DATE_FORMAT(sales.date,'%d %b %y') as date, sales.quantity as Quantity, sales.prod_id as prod_id,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
+ 	        	if (err) 
+ 	        		return next(err);
+ 		var query = 'SELECT name,id from products';
+ 		//var query = 'SELECT sales.prod_id as id,products.name as name from sales,products WHERE products.id = sales.prod_id group by name';
+ 		connection.query(query, [], function(err, results1){
+ 				res.render( 'addSaleScreen', {
+ 					sales : results,
+ 					products : results1
+ 				});
+ 			});
+ 		});
+ 	});
+});
+};
+exports.showEdit = function (req, res, next) {
+ 
+ 		req.getConnection(function(err, connection){
+ 		if (err)
+ 			return next(err);
+ 	
+			connection.query("SELECT DATE_FORMAT(sales.date,'%d %b %y') as date, sales.quantity as Quantity, sales.prod_id as id,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
+			connection.query("SELECT sales.id, DATE_FORMAT(sales.date,'%d %b %y') as date, sales.quantity as Quantity, sales.prod_id as prod_id ,products.name as name,sum(sales.quantity * sales.price) as sales from sales, products WHERE products.id = sales.prod_id group by name order by sum(sales.quantity*sales.price) DESC", [], function(err, results) {
+ 	        	if (err) 
+ 	        		return next(err);
+ 		var query = 'SELECT name,id from products';
+ 		//var query = 'SELECT sales.prod_id as id,products.name as name from sales,products WHERE products.id = sales.prod_id group by name';
+ 		connection.query(query, [], function(err, results1){
+ 				res.render( 'editSales', {
+ 					sales : results,
+ 					products : results1
+ 				});
+ 			});
+ 		});
+ 	});
+    });
+};
+exports.addsale = function (req, res, next) {
+ 	req.getConnection(function(err, connection){
+ 		if (err){
+ 			return next(err);
+ 		}
+ 		var input = JSON.parse(JSON.stringify(req.body));
+ 		var data = {
+ 			prod_id: input.prod_id,
+ 			date: input.sales_date,
+ 			quantity: input.quantity,
+ 			price : input.price
+ 		};
+ 		connection.query('insert into sales set ?', data, function(err, results) {
+ 			if (err)
+ 			    console.log("Error inserting : %s ",err );
+ 
+ 			res.redirect('/sales');
+ 		});
+ 	});
+};
+ 
+ 
+exports.get = function(req, res, next){
+ 	var id = req.params.id;
+ 	var input = JSON.parse(JSON.stringify(req.body));
+ 	    var data = {
+ 			prod_id: input.id,
+ 			date: input.date,
+ 			quantity: input.quantity,
+ 			price : input.price
+ 		};
+ 	req.getConnection(function(err, connection){
+ 		connection.query('SELECT * FROM sales WHERE id = ?', [id], function(err,rows){
+ 			if(err){
+ 		        console.log("Error Selecting : %s ",err );
+ 			}
+ 		    res.render('editSales',{page_title:"Edit Customers - Node.js", data : rows[0]});
+ 		});
+ 	});
+};
+ 
+exports.salesUpdate = function(req, res, next){
+ 	var data = JSON.parse(JSON.stringify(req.body));
+ 	var id = req.params.id;
+ 	var input = JSON.parse(JSON.stringify(req.body));
+ 	     var data = {
 			prod_id: input.id,
-			date: input.sales_date,
+			date: input.date,
 			quantity: input.quantity,
-			price : input.price
->>>>>>> b2027164d9547aa5e23c51cd0967df787fb8fadc
-		};
-	req.getConnection(function(err, connection){
-		connection.query('UPDATE sales SET ? WHERE id = ?', [data, id], function(err){
-			if (err){
-		       console.log("Error Updating : %s ",err );
-			}
-		    res.redirect('/sales');
-		});
-	});
+			price : input.price,
+			name: input.name
+			//date: input.date,
+			//quantity: input.quantity,
+			//price : input.price
+ 		};
+ 	req.getConnection(function(err, connection){
+ 		connection.query('UPDATE sales SET ? WHERE id = ?', [data, id], function(err, rows){
+ 			if (err){
+ 		       console.log("Error Updating : %s ",err );
+ 			}
+ 		    res.redirect('/sales');
+ 		});
+ 	});
 };
-exports.delete = function(req, res, next){
-	var id = req.params.id;
-	req.getConnection(function(err, connection){
-	    connection.query('DELETE FROM sales WHERE id = ?', [id], function(err,rows){
-	        if(err){
-	            console.log("Error Selecting : %s ",err );
-	            return res.redirect('/categories?error=true&msg=category_linked');
-		    }
-		    else{
-	        	res.redirect('/sales');
-	        }
-		});
-	});
-};
-
+ exports.delete = function(req, res, next){
+ 	var id = req.params.id;
+ 	req.getConnection(function(err, connection){
+ 	    connection.query('DELETE FROM sales WHERE Id = ?', [id], function(err,rows){
+ 	        if(err){
+ 	            console.log("Error Selecting : %s ",err );
+ 	            return res.redirect('/categories?error=true&msg=category_linked');
+ 		    }
+ 		    else{
+ 	        	res.redirect('/sales');
+ 	        }
+ 		});
+ 	});
+ };
