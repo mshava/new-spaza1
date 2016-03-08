60ab4230e5913c5d3442e807a7c07b0049560570
mshava <Sakhile@projectCodeX.co>
2016-03-08 09:59:02 +0200

    categories data services done

--- data_services/categoriesDataService.js
+++ data_services/categoriesDataService.js
@@ -7,4 +7,25 @@ module.exports = function(connection) {
   this.showCategories = function() {
       return queryService.executeQuery('SELECT  * FROM categories');
   };
+
+  this.showMostPopCat = function() {
+    return queryService.executeQuery('SELECT  categories.name, sum(sales.quantity) as totalqty FROM sales INNER JOIN products ON sales.prod_id = products.id INNER JOIN categories ON products.cat_id = categories.id GROUP BY categories.name ORDER BY totalqty DESC LIMIT 0,1; ');
+  };
+
+  this.addCategories = function(data) {
+    return.executeQuery('INSERT INTO categories set ?', data);
+  };
+
+  this.getCategories = functiono(id) {
+    return queryService.executeQuery('SELECT * FROM categories WHERE id = ?', id);
+  };
+
+  this.updateCategories = function(data, id) {
+    return queryService.executeQuery('UPDATE categories set WHERE id = ?', data, id);
+  };
+
+  this.deleteCategories = function(id) {
+    return queryService.executeQuery('DELETE FROM categories WHERE id = ?', id);
+  };
+
 };
