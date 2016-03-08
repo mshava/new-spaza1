bb1b73e7bd74350a12f394553aeaadbfaa1a2bca
mshava <Sakhile@projectCodeX.co>
2016-03-08 15:45:57 +0200

    created purchases queries

--- data_services/purchasesDataService.js
+++ data_services/purchasesDataService.js
@@ -12,4 +12,12 @@ module.exports = function(connection) {
     return queryService.executeQuery('SELECT * FROM purchases');
   };
 
+  this.showAddPurchases = function() {
+    return queryService.executeQuery('SELECT purchases.id as id,DATE_FORMAT(purchases.purchase_date,'%d %b %y') as date,purchases.quantity as quantity,purchases.cost as price, products.name as product from purchases,products where products.id = purchases.prod_id order by purchases.purchase_date DESC');
+  };
+
+  this.addPurchases = function() {
+    return queryService.executeQuery('SELECT purchases.id as id,DATE_FORMAT(purchases.purchase_date,'%d %b %y') as date,purchases.quantity as quantity,purchases.cost as price, products.name as product from purchases,products where products.id = purchases.prod_id order by purchases.purchase_date DESC');
+  };
+
 };
